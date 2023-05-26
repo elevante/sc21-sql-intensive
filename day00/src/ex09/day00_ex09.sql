4752d74010a67d7cb747cd6a1be9e1fe1f7e5985
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-05-26 18:08:47 +0300

    day01/wip: finished ex00

--- day00/src/ex09/day00_ex09.sql
+++ day00/src/ex09/day00_ex09.sql
@@ -4,7 +4,6 @@ SELECT (SELECT name
        (SELECT pizza_name
         FROM menu
         WHERE id = pv.pizzeria_id) AS pizzeria_name
-
 FROM (SELECT person_id, pizzeria_id
       FROM person_visits
       WHERE visit_date BETWEEN '2022-01-07' AND '2022-01-09') AS pv
