c07dcbd3f30dafe7d99d52a27b5f143c455772f9
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-05-31 23:43:30 +0300

    day01/wip: finished ex03

---
+++ day02/src/ex02/day02_ex02.sql
@@ -0,0 +1,20 @@
+SELECT
+  COALESCE(p.name, '-') AS person_name,
+  visit_date AS person_visit,
+  COALESCE(piz.name, '-') AS pizzeria_name
+FROM
+  person as p FULL
+  JOIN (
+    SELECT
+      *
+    FROM
+      person_visits
+    WHERE
+      visit_date BETWEEN '2022-01-01'
+      AND '2022-01-03'
+  ) AS pv ON pv.person_id = p.id FUll
+  JOIN pizzeria piz ON piz.id = pv.pizzeria_id
+ORDER BY
+  person_name,
+  visit_date,
+  pizzeria_name;
