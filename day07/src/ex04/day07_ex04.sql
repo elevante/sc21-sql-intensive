65158ead95ac8813dc1f1f9b8ead5a06d46ecfb5
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-06-08 15:50:48 +0300

    day07/wip: finished ex05

--- day07/src/ex04/day07_ex04.sql
+++ day07/src/ex04/day07_ex04.sql
@@ -2,4 +2,4 @@ SELECT p.name, COUNT(visit_date) AS count_of_visits
 FROM person p
          JOIN person_visits pv ON p.id = pv.person_id
 GROUP BY p.name
-HAVING COUNT(visit_date) > 3
+HAVING COUNT(visit_date) > 3;
